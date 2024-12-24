6a51cdd362019b77433812ef3518e01609392049
git-Hpiuu <huy12500@gmail.com>
2024-12-24 14:47:07 +0700

    Add files via upload

---
+++ admin_script.js
@@ -0,0 +1,23 @@
+let navbar = document.querySelector('.header .navbar');
+let accountBox = document.querySelector('.header .account-box');
+
+document.querySelector('#menu-btn').onclick = () =>{
+   navbar.classList.toggle('active');
+   accountBox.classList.remove('active');
+}
+
+document.querySelector('#user-btn').onclick = () =>{
+   accountBox.classList.toggle('active');
+   navbar.classList.remove('active');
+}
+
+window.onscroll = () =>{
+   navbar.classList.remove('active');
+   accountBox.classList.remove('active');
+}
+
+document.querySelectorAll('.show-products .box-container .box .delete-btn').forEach(
+   button => { button.addEventListener('click', function() { 
+      this.closest('.show-products .box-container .box').style.display = 'none';
+      })
+   })
